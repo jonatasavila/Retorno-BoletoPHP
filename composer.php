436b5de0dd0cf0e405159a2448615ac9821f026b
jonatas <jonatas@webjump.com.br>
2018-06-10 04:55:10 -0300

    creating version

---
+++ composer.php
@@ -0,0 +1,18 @@
+{
+    "name": "jonatasavila/retorno-boleto-php",
+    "description": "",
+    "type": "project",
+    "version": "1.0.0",
+    "license": [
+        "proprietary"
+    ],
+    "config": {
+        "use-include-path": true
+    },
+    "minimum-stability": "alpha",
+    "prefer-stable": true,
+    "extra": {
+        "magento-force": "override"
+    }
+}
+
